9f768ce33e260564356e723194898912218f5bd1
Федоров Максим Викторович <fedorovmv@pgk.ru>
2023-05-31 13:53:32 +0300

    feat: guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from "../shared/guards/auth.guard";
+import { NotAuthorizedGuard } from "../shared/guards/not-authorized.guard";
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'auth', loadChildren: () => import('auth/auth.module').then(m => m.AuthModule) },
-    { path: 'home', loadChildren: () => import('app/home/home.module').then(m => m.HomeModule) },
-    { path: 'inventory', loadChildren: () => import('app/inventory/inventory.module').then(m => m.InventoryModule) },
-    { path: 'reports', loadChildren: () => import('app/reports/reports.module').then(m => m.ReportsModule) },
-    { path: 'billing', loadChildren: () => import('app/billing/billing.module').then(m => m.BillingModule) },
-    { path: 'profile', loadChildren: () => import('app/profile/profile.module').then(m => m.ProfileModule) },
+    { path: 'auth', canActivate: [NotAuthorizedGuard], loadChildren: () => import('auth/auth.module').then(m => m.AuthModule) },
+    { path: 'home', canActivate: [AuthGuard], loadChildren: () => import('app/home/home.module').then(m => m.HomeModule) },
+    { path: 'inventory', canActivate: [AuthGuard], loadChildren: () => import('app/inventory/inventory.module').then(m => m.InventoryModule) },
+    { path: 'reports', canActivate: [AuthGuard], loadChildren: () => import('app/reports/reports.module').then(m => m.ReportsModule) },
+    { path: 'billing', canActivate: [AuthGuard], loadChildren: () => import('app/billing/billing.module').then(m => m.BillingModule) },
+    { path: 'profile', canActivate: [AuthGuard], loadChildren: () => import('app/profile/profile.module').then(m => m.ProfileModule) },
 ];
 
 @NgModule({
---
+++ src/shared/guards/not-authorized.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ProfileService } from "../root-services/profile.service";
+import { RouterService } from "../root-services/router.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotAuthorizedGuard implements CanActivate {
+    constructor(
+        private profileService: ProfileService,
+        private router: RouterService,
+    ) {}
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+        if (!this.profileService.profile$.value) {
+            return true;
+        } else {
+            this.router.goToApp()
+            return false
+        }
+    }
+}
